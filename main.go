b4430a75a740ecdc5aadaa54344e415f8d311d4e
Karsen H <karsenhansen@gmail.com>
2024-09-02 22:34:52 -0700

    Added functionality to scrape and build stock struct

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+
+
+type Stock struct{
+	company, price, change string
+}
+
+func main() {
+	ticker := []string{
+		"MSFT",
+		"IBM",
+		"GE",
+	}
+
+	stocks := []Stock{}
+
+	c := colly.NewCollector()
+	c.OnRequest((func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla 5.0")
+		fmt.Println("Visiting:", r.URL)
+	}))
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong: ", err)
+	})
+
+	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
+		stock := Stock{}
+
+		stock.company = e.ChildText("h1")
+		fmt.Println("Company: ", stock.company)
+		stock.price = e.ChildText("fin-streamer[data-field]='regularMarketPrice']")
+		fmt.Println("Price: ", stock.price)
+		stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
+		fmt.Println("Change: ", stock.change)
+
+		stocks = append(stocks, stock) 
+	})
+
+	c.Wait()
+
+	for _, t := range ticker {
+		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
+	}
+
+	println(ticker)
+	println(stocks)
+}
